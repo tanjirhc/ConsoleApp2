1323173a76dd9ae17f9579a8c6d13e52706f785e
Tanjir <tanjirhc@gmail.com>
2024-01-27 20:48:53 +0600

    Revert "Keeping one message"
    This reverts commit 75a0b63a60b988b2d9461dc34a9d2cd9b5521f03.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy! One more Modification");
-
+Console.WriteLine("Hello, C# Academy! One more Modification2");
+Console.WriteLine("Hello, C# Academy! One more Modification3");
 
 
 Console.ReadLine();
