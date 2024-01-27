bfa5e75d36ee80e23f3ea3ff67503b6e23205358
Tanjir <tanjirhc@gmail.com>
2024-01-27 20:36:16 +0600

    Console.WritLine Modified

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,10 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string[] arrStr = { "ASP", "MVC", "CORE" };
-int i = 0;
-
-//Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"));
-DateTime ptoday = DateTime.ParseExact("26/01/2024", "dd/MM/yyyy hh:mm",null);
-Console.WriteLine(ptoday.ToString());
+Console.WriteLine("Hello, C# Academy!");
 
 
 Console.ReadLine();
