75a0b63a60b988b2d9461dc34a9d2cd9b5521f03
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-01-27 20:44:34 +0600

    Keeping one message

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy! One more Modification");
-Console.WriteLine("Hello, C# Academy! One more Modification2");
-Console.WriteLine("Hello, C# Academy! One more Modification3");
+
 
 
 Console.ReadLine();
